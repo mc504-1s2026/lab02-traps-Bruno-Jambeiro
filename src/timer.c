ef62356f40cd8dc879640b505388c1835711a1fa
Bruno Jambeiro <brunojambeirom@gmail.com>
2026-07-14 21:05:10 -0300

    Finalizado gráças a Deus

--- src/timer.c
+++ src/timer.c
@@ -1,32 +1,41 @@
+#include <arch/csr.h>
 #include <arch/timer.h>
 #include <kernel/panic.h>
+#include <kernel/serial.h>
+
+static volatile u8 alarm_pending = 0;
 
 u64 timer_read()
 {
-	/* not implemented */
-	BUG();
+	return csr_read(CSR_TIME);
 }
 
 void timer_irq_enable()
 {
-	/* not implemented */
-	BUG();
+	csr_set(CSR_SIE, CSR_SIE_STIE);
 }
 
 void timer_irq_disable()
 {
-	/* not implemented */
-	BUG();
+	csr_clear(CSR_SIE, CSR_SIE_STIE);
 }
 
 void timer_set_alarm(u64 secs)
 {
-	/* not implemented */
-	BUG();
+	u64 now = timer_read();
+	u64 deadline = now + (secs * TIMER_FREQ);
+
+	alarm_pending = 1;
+	csr_write(CSR_STIMECMP, deadline);
+	timer_irq_enable();
 }
 
 void timer_irq()
 {
-	/* not implemented */
-	BUG();
+	if (alarm_pending) {
+		alarm_pending = 0;
+		serial_puts("alarm\r\n");
+	}
+
+	timer_irq_disable();
 }
--- src/trap.c
+++ src/trap.c
@@ -1,53 +1,76 @@
+#include <arch/csr.h>
+#include <arch/timer.h>
 #include <kernel/trap.h>
 #include <kernel/panic.h>
+#include <kernel/serial.h>
 
 /* defined in src/trap_entry.S */
 extern void trap_entry();
 
 void handle_irq()
 {
-	/* not implemented */
-	BUG();
+	u64 cause = csr_read(CSR_SCAUSE) & ~TRAP_IRQ_BIT;
+
+	switch (cause) {
+	case TRAP_TIMER_IRQ & ~TRAP_IRQ_BIT:
+		timer_irq();
+		break;
+	case TRAP_EXTERNAL_IRQ & ~TRAP_IRQ_BIT:
+		serial_irq();
+		break;
+	default:
+		panic("unhandled irq cause 0x%x\n", cause);
+	}
 }
 
 void handle_exception()
 {
-	/* not implemented */
-	BUG();
+	u64 cause = csr_read(CSR_SCAUSE);
+	u64 stval = csr_read(CSR_STVAL);
+	u64 sepc = csr_read(CSR_SEPC);
+
+	panic("unhandled exception cause=0x%x stval=0x%x sepc=0x%x\n",
+	      cause, stval, sepc);
 }
 
 void trap_setup()
 {
-	/* not implemented */
-	BUG();
+	csr_write(CSR_STVEC, (u64)trap_entry);
+	csr_clear(CSR_SSTATUS, CSR_SSTATUS_SIE);
+	csr_write(CSR_SIE, 0);
 }
 
 void handle_trap()
 {
-	/* not implemented */
-	BUG();
+	u64 cause = csr_read(CSR_SCAUSE);
+
+	if (cause & TRAP_IRQ_BIT)
+		handle_irq();
+	else
+		handle_exception();
 }
 
 void hart_irq_enable()
 {
-	/* not implemented */
-	BUG();
+	csr_set(CSR_SSTATUS, CSR_SSTATUS_SIE);
 }
 
 u64 hart_irq_save()
 {
-	/* not implemented */
-	BUG();
+	u64 flags = csr_read(CSR_SSTATUS) & CSR_SSTATUS_SIE;
+	csr_clear(CSR_SSTATUS, CSR_SSTATUS_SIE);
+	return flags;
 }
 
 void hart_irq_restore(u64 flags)
 {
-	/* not implemented */
-	BUG();
+	if (flags & CSR_SSTATUS_SIE)
+		csr_set(CSR_SSTATUS, CSR_SSTATUS_SIE);
+	else
+		csr_clear(CSR_SSTATUS, CSR_SSTATUS_SIE);
 }
 
 void hart_irq_disable()
 {
-	/* not implemented */
-	BUG();
+	csr_clear(CSR_SSTATUS, CSR_SSTATUS_SIE);
 }
